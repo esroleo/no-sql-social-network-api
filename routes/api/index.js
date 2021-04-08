a11c66d9b6c00f6fb1f565d58cec68bd02177e36
esroleo <esroleo@gmail.com>
2021-04-08 16:13:04 -0400

    server.js populated

---
+++ routes/api/index.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const thoughtRoutes = require('./thought-routes.js');
+const userRoutes = require('./user-routes.js');
+
+// add prefix of `/pizzas` to routes created in `pizza-routes.js`
+router.use('/comments', commentRoutes);
+router.use('/pizzas', pizzaRoutes);
+
+
+module.exports = router;
